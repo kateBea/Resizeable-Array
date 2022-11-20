8b61effd6fc3ea7cf16c1362b5fa4c64e3b92eaf
ケイト <55351037+kateBea@users.noreply.github.com>
2022-11-20 07:11:01 +0100

    reallocate, constructors rework & some fixes

--- src/vector.h
+++ src/vector.h
@@ -1,7 +1,7 @@
-#ifndef VECTOR_H
-#define VECTOR_H
+#ifndef VECTOR_HH
+#define VECTOR_HH
 
-#include <iostream>
+#include <cstdio>
 #include <new>
 #include <cstring>
 #include <exception>
@@ -17,13 +17,14 @@
 
 namespace kt
 {
-template <typename T>
+template
+<typename T, typename alloc = std::allocator<T>>
 class vector
 {
 public:
     using size_type = std::size_t;
     using pointer_type = T*;
-    using const_pointer_type = const T*;
+    using pointer_to_const_type = const T*;
 
     ///
     /// default constructor
@@ -35,62 +36,64 @@ public:
     }
 
     ///
-    /// parametrized constructor. Initialize vector with count elements
+    /// parametrized constructor. Reserve space to hold at least "count" elements
     ///
     vector(size_type count)
-        :   m_array{ nullptr }, m_count{ count }, m_capacity{ count }
+        :   m_array{ nullptr }, m_count{ 0 }, m_capacity{ count }
     {
         if (count != 0)
-        {
-            this->m_array = new (std::nothrow) T[this->m_count];
-        }
+            this->m_array = static_cast<pointer_type>(::operator new(sizeof(T) * count, std::nothrow));
     }
 
     ///
-    /// parametrized constructor. Initializes the vector
-    /// with the elements from the list
+    /// parametrized constructor. Initializes vector
+    /// with the elements from the "content"
     ///
     vector(std::initializer_list<T>&& content)
-        :   m_array{ new (std::nothrow) T[content.size()] }, m_count{ content.size() }, m_capacity{ content.size() }
+        :   m_array{ static_cast<pointer_type>(::operator new(sizeof(T) * content.size(), std::nothrow)) },
+            m_count{ content.size() }, m_capacity{ content.size() }
     {
         if (this->m_array)
             std::copy(content.begin(), content.end(), this->m_array);
     }
 
     ///
-    /// copy constructor. Deep copy of p_vector
+    /// copy constructor. Initialize this vector with elements of "other"
     ///
-    vector(const vector& p_vector)
+    vector(const vector& other)
         :   m_array{ nullptr }, m_count{}, m_capacity{}
     {
-        if (p_vector.m_count != 0)
+        if (other.m_count != 0)
         {
-            this->m_array = new (std::nothrow) T[p_vector.m_count];
-            this->m_count = p_vector.m_count;
-            this->m_capacity = p_vector.m_capacity;
+            this->m_array = static_cast<pointer_type>(::operator new(sizeof(T) * other.m_count, std::nothrow));
+
+
+            this->m_count = other.m_count;
+            this->m_capacity = other.m_capacity;
 
             if (this->m_array != nullptr)
-                std::copy(p_vector.m_array, p_vector.m_array + p_vector.m_count,
-                    this->m_array);
+                std::copy(other.m_array, other.m_array + other.m_count, this->m_array);
         }
     }
 
     ///
     /// assigment operator. Deep copy of p_vector
     ///
-    vector& operator=(const vector& p_vector)
+    vector& operator=(const vector& other)
     {
-        if (this != &p_vector)
+        if (this != &other)
         {
             // clean up previous block
-            delete[] this->m_array;
+            for (size_type index{}; index < m_count; ++index)
+                this->m_array[index].~T();
+            ::operator delete(this->m_array);
 
-            this->m_array = new (std::nothrow) T[p_vector.m_count];
-            this->m_count = p_vector.m_count;
+            this->m_array = static_cast<pointer_type>(::operator new(sizeof(T) * other.m_count, std::nothrow));
+            this->m_count = other.m_count;
+            this->m_capacity = other.m_capacity;
 
-            if (this->m_array != nullptr)
-                std::copy(p_vector.m_array, p_vector.m_array + p_vector.m_count,
-                    this->m_array);
+            if (this->m_array)
+                std::copy(other.m_array, other.m_array + other.m_count, this->m_array);
         }
 
         return *this;
@@ -99,13 +102,14 @@ public:
     ///
     /// move constructor
     ///
-    vector(vector&& p_vector)
-        :   m_array{ p_vector.m_array }, m_count{ p_vector.m_count }
+    vector(vector&& other)
+        :   m_array{ other.m_array }, m_count{ other.m_count }, m_capacity{ other.m_capacity }
     {
-        if (p_vector.m_count != 0)
+        if (other.m_capacity != 0)
         {
-            p_vector.m_array = nullptr;
-            p_vector.m_count = 0;
+            other.m_array = nullptr;
+            other.m_count = 0;
+            other.m_capacity = 0;
         }
     }
 
@@ -115,21 +119,27 @@ public:
     ~vector()
     {
         // cleanup
-        delete[] this->m_array;
+        for (size_type index{}; index < m_count; ++index)
+            this->m_array[index].~T();
+
+        ::operator delete(this->m_array);
+        // delete[] this->m_array;
     }
 
     ///
     /// assigment operator
     ///
-    vector& operator=(vector&& p_vector)
+    vector& operator=(vector&& other)
     {
-        if (this != &p_vector)
+        if (this != &other)
         {
-            this->m_array = p_vector.m_array;
-            this->m_count = p_vector.m_count;
+            this->m_array = other.m_array;
+            this->m_count = other.m_count;
+            this->m_capacity = other.m_capacity;
 
-            p_vector.m_array = nullptr;
-            p_vector.m_count = 0;
+            other.m_array = nullptr;
+            other.m_count = 0;
+            other.m_capacity = 0;
         }
 
         return *this;
@@ -152,11 +162,11 @@ public:
     }
 
     ///
-    /// return true if the vector is empty, false otherwise
+    /// return true if the vector is has no elements
     ///
     auto empty() const -> bool
     {
-        return this->m_array == nullptr;
+        return this->m_count == 0;
     }
 
     ///
@@ -189,9 +199,9 @@ public:
                 throw out_of_bounds{};
 
         }
-        catch (const out_of_bounds& oob) { std::cerr << oob.what() << '\n'; }
-        catch (const empty_vector& ema) { std::cerr << ema.what() << '\n'; }
-        catch(...) { std::cerr << "Other exceptions thrown\n"; }
+        catch (const out_of_bounds& oob) { std::printf("%s", oob.what()); }
+        catch (const empty_vector& ema) { std::printf("%s", ema.what()); }
+        catch(...) { std::printf("Other exceptions thrown"); }
 
         return this->m_array[index];
     }
@@ -224,14 +234,14 @@ public:
         // increase count by 1
         if (this->m_capacity > this->m_count)
         {
-            m_array[m_count] = info;
+            this->m_array[this->m_count] = info;
             this->m_count += 1;
         }
         else
         {
             reallocate();
 
-            this->m_array[m_count] = info;
+            this->m_array[this->m_count] = info;
             this->m_count += 1;
         }
     }
@@ -253,18 +263,18 @@ public:
         // increase count by 1
         if (this->m_capacity > this->m_count)
         {
-            m_array[m_count] = std::move(info);
+            m_array[this->m_count] = std::move(info);
             this->m_count += 1;
         }
         else
         {
             reallocate();
 
-            this->m_array[m_count] = std::move(info);
+            this->m_array[this->m_count] = std::move(info);
             this->m_count += 1;
         }
     }
-    
+
     ///
     /// Remove the last element from the vector
     ///
@@ -278,13 +288,13 @@ public:
         }
 
     }
-    
+
     ///
     /// Remove all elements of the vector
     ///
     auto clear() -> void
     {
-        std::for_each(m_array, m_array + m_count, [](T& info) -> void { info.~T(); });
+        std::for_each(this->m_array, this->m_array + this->m_count, [](T& info) -> void { info.~T(); });
 
         // std::destroy_n(this->m_array, this->m_count);
 
@@ -293,11 +303,11 @@ public:
 
         this->m_count = 0;
     }
-    
+
     ///
     /// Ajust vector to contain count elements
     ///
-    auto resize(size_type block_size) -> void;
+    auto resize(size_type count) -> void;
 
     ///
     /// Returns a pointer to the beginning of the vector
@@ -337,17 +347,17 @@ private:
     void reallocate()
     {
         size_type new_block_size{ (!this->m_capacity) ? 1 : (m_capacity * grow_factor) };
-        pointer_type new_block{ new (std::nothrow) T[new_block_size] };
+        pointer_type new_block{ static_cast<pointer_type>(::operator new(sizeof(T) * new_block_size, std::nothrow)) };
 
         if (not new_block)
         {
-            std::cerr << "Failed to allocate new block of memory\n";
+            std::printf("Failed to allocate new block of memory");
             return;
         }
 
         std::copy(this->m_array, this->m_array + this->m_count, new_block);
 
-        delete[] this->m_array;
+        ::operator delete(static_cast<void*>(this->m_array));
 
         this->m_array = new_block;
         this->m_capacity = new_block_size;
@@ -394,4 +404,4 @@ private:
 
 }   // END KATE NAMESPACE
 
-#endif // END VECTOR
+#endif
